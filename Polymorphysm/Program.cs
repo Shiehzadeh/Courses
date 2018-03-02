16a1b1de8f6860a156d7e78137344824a8bf32ab
Shiehzadeh <36985867+Shiehzadeh@users.noreply.github.com>
2018-03-02 13:01:44 +0330

    first commit

---
+++ Polymorphysm/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphysm
+{
+    class Program
+    {
+        #region public  variable
+        static int rWidth;
+        static int rLength;
+        static int sSide;
+        static double cRadius;
+        #endregion
+        static void Main(string[] args)
+        {
+            GetInput();
+            Initial();
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// گرفتن اطلاعات از کاربر
+        /// </summary>
+        static void GetInput()
+        {
+            Console.Write("please enter Rectangle Width: ");
+            rWidth = Convert.ToInt32(Console.ReadLine());
+            Console.Write("please enter Rectangle Length: ");
+            rLength = Convert.ToInt32(Console.ReadLine());
+            Console.Write("please enter Square Side: ");
+            sSide = Convert.ToInt32(Console.ReadLine());
+            Console.Write("please enter Circle Radius: ");
+            cRadius = Convert.ToInt32(Console.ReadLine());
+        }
+        /// <summary>
+        /// تجزیه تحلیل و محاسبه محیط و مساحت اشکال
+        /// </summary>
+        static void Initial()
+        {
+            Rectangle r = new Rectangle(rWidth, rLength);
+            Squar s = new Squar(sSide);
+            Circle c = new Circle(cRadius);
+            double rA =r.Area();
+            double rE = r.Environment();
+            double sA = s.Area();
+            double sE = r.Environment();
+            double cA = s.Area();
+            double cE = r.Environment();
+            Console.WriteLine($"Rectangle area is:{rA}");
+            Console.WriteLine($"Rectangle Environment is:{rE}");
+            Console.WriteLine($"Squar area is:{sA}");
+            Console.WriteLine($"Squar Environment is:{sE}");
+            Console.WriteLine($"Circle area is:{cA}");
+            Console.WriteLine($"Circle Environment is:{cE}");
+
+
+        }
+    }
+}
---
+++ Polymorphysm/Shap.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphysm
+{
+    public class Shap
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Side { get; set; }
+        public double Diameter { get; set; }
+        public double P =Math.PI;
+        public Shap(int w, int L)
+        {
+            Length = L;
+            Width = w;
+        }
+        public Shap(int s)
+        {
+            Side = s;
+        }
+        public Shap(double c)
+        {
+            Diameter = c;
+        }
+        //محاسبه مساحت مستطیل و مربع
+        public virtual double Area()
+        {
+            return 0;
+        }
+        public virtual double Environment()
+        {
+            return 0;
+        }
+    }
+    public class Rectangle:Shap
+    {
+        public Rectangle(int w,int l): base(w, l)
+        {
+        }
+        public override double Area()
+        {
+            return Width * Length;
+        }
+        public override double Environment()
+        {
+            return (Width + Length) * 2;
+        }
+    }
+    public class Squar : Shap
+    {
+        public Squar(int s) : base(s)
+        {
+        }
+        public override double Area()
+        {
+            return Side * Side;
+        }
+        public override double Environment()
+        {
+            return Side * 4;
+        }
+    }
+    public class Circle : Shap
+    {
+        public Circle(double c) : base(c)
+        {
+        }
+        public override double Area()
+        {
+            return Diameter * P;
+        }
+        public override double Environment()
+        {
+            return (Diameter / 2)*2*P;
+        }
+    }
+}
